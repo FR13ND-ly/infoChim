a253aba4a050577206adb2c13c7110c326f900bc
Alan <57838113+FR13ND-ly@users.noreply.github.com>
2022-05-21 17:23:15 +0300

    added tooltips to

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -6,6 +6,7 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [],
@@ -18,7 +19,8 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatMenuModule,
     MatFormFieldModule,
     MatInputModule,
-    MatDialogModule
+    MatDialogModule,
+    MatTooltipModule
   ]
 })
 export class MaterialModule { }
