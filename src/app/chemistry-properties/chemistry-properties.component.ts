d5f836fcd7cba92bd1d5d863e5f4737d8247d2cc
Alan <57838113+FR13ND-ly@users.noreply.github.com>
2022-05-22 14:21:21 +0300

    button for showing examples

--- src/app/chemistry-properties/chemistry-properties.component.ts
+++ src/app/chemistry-properties/chemistry-properties.component.ts
@@ -1,18 +1,185 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Title } from '@angular/platform-browser';
+import { DomSanitizer, Title } from '@angular/platform-browser';
 import { VideoComponent } from '../video/video.component';
 
 @Component({
   selector: 'app-chemistry-properties',
   templateUrl: './chemistry-properties.component.html',
-  styleUrls: ['./chemistry-properties.component.scss']
+  styleUrls: ['./chemistry-properties.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ChemistryPropertiesComponent implements OnInit {
 
-  constructor(public dialog: MatDialog, private titleService: Title) { }
+  constructor(public dialog: MatDialog, private titleService: Title, public sanitizer: DomSanitizer) { }
 
   titles : any = []
+
+  R: any = [
+    {
+      reacts : [
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          2R-COOH+Na<sub>2</sub>CO<sub>3</sub>→2R-CO<span class="negative">O</span>N<span class="positive">a</span>
+          +CO<sub>2</sub>↑+H<sub>2</sub>O
+        </div>`),
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="blue">CH<sub>3</sub>-COOH</span>+<span class="teal">NaHCO<sub>3</sub></span>→<span class="red">CH<sub>3</sub>-CO<span class="negative">O</span>N<span class="positive">a</span><sub>4</sub></span>
+          +CO<sub>2</sub>↑+H<sub>2</sub>O
+        </div>
+        <p class="text">
+            <span class="blue">Acid Acetic</span>
+            <span class="teal bicarbonat">Bicarbonat de Sodiu(praf de copt)</span>
+            <span class="red acetat-4">Acetat de Sodiu</span>
+        </p>`)
+      ],
+      video : "K8xfoUWuCfQ",
+      ex : 0
+    },
+    {
+      reacts : [
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          2R-COOH-CaCO<sub>3</sub>→(R-CO<span class="negative">O</span>)<sub>2</sub>C<span class="positive-2">a</span>
+          +CO<sub>2</sub>↑+H<sub>2</sub>O
+        </div>`),
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+            <span class="green">2CH<sub>3</sub>-COOH</span>+<span class="yellow">CaCO<sub>3</sub></span>→<span class="purple">(CH<sub>3</sub>-CO<span class="negative">O</span>)<sub>2</sub>C<span class="positive-2">a</span></span>
+            +CO<sub>2</sub>↑+H<sub>2</sub>O
+          </div>
+          <p class="text">
+              <span class="green">Acid Acetic</span>
+              <span class="yellow carbonat">Carbonat de Calciu</span>
+              <span class="purple acetat-3">Acetat de Calciu</span>
+          </p>`)
+      ],
+      video : "spsaNMBtaF0",
+      ex : 0
+    },
+    {
+      reacts : [
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="blue">R-COOH</span>+<span class="red">Na</span>→<span class="green">R-CO<span class="negative">O</span>N<span class="positive">a</span></span>+
+          <div class="fraction">
+              <span>1</span>
+              <span class="fraction__divider"></span>
+              <span>2</span>
+          </div>
+          H<sub>2</sub>↑
+        </div>
+        <p class="text green sare-1">Sare organică de natriu</p>`),
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="blue">CH<sub>3</sub>-COOH</span>+<span class="red">Na</span>→<span class="green">CH<sub>3</sub>-CO<span class="negative">O</span>N<span class="positive">a</span></span>+
+          <div class="fraction">
+              <span>1</span>
+              <span class="fraction__divider"></span>
+              <span>2</span>
+          </div>
+          H<sub>2</sub>↑
+        </div>
+        <p class="text"><span class="blue">Acid Acetic</span><span class="green acetat-2">Acetat de Sodiu</span></p>`)
+      ],
+      video : "aGaWwnMyKVE",
+      ex : 0
+    },
+    {
+      reacts : [
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="blue">R-COOH</span>+<span class="red">Na</span>→<span class="green">R-CO<span class="negative">O</span>N<span class="positive">a</span></span>+
+          <div class="fraction">
+              <span>1</span>
+              <span class="fraction__divider"></span>
+              <span>2</span>
+          </div>
+          H<sub>2</sub>↑
+        </div>
+        <p class="text green sare-1">Sare organică de natriu</p>`),
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="blue">CH<sub>3</sub>-COOH</span>+<span class="red">Na</span>→<span class="green">CH<sub>3</sub>-CO<span class="negative">O</span>N<span class="positive">a</span></span>+
+          <div class="fraction">
+              <span>1</span>
+              <span class="fraction__divider"></span>
+              <span>2</span>
+          </div>
+          H<sub>2</sub>↑
+        </div>
+        <p class="text"><span class="blue">Acid Acetic</span><span class="green acetat-2">Acetat de Sodiu</span></p>`)
+      ],
+      video : "aGaWwnMyKVE",
+      ex : 0
+    },
+    {
+      reacts : [
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="blue">R-COOH</span>+
+          <span class="teal">R'-OH</span>→
+          <span class="purple">R-COOR'</span>
+          +H<sub>2</sub>O
+        </div>
+        <p class="text">
+            <span class="blue">Acid Organic </span>
+            <span class="teal alcool-2">Alcool </span>
+            <span class="purple ester">Ester(sare organică)</span>
+        </p>`),
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="blue">CH<sub>3</sub>-COOH</span>+<span class="teal">C<sub>2</sub>H<sub>5</sub>-OH</span>→<span class="purple">CH<sub>3</sub>-COOC<sub>2</sub>H<sub>5</sub></span>+H<sub>2</sub>O
+        </div>
+        <p class="text"><span class="blue">Acid Acetic</span><span class="teal alcool-1">Alcool Etilic</span><span class="purple acetat-1">Acetat de Etil</span></p>`)
+      ],
+      video : "id7Fve9cMIw",
+      ex : 0
+    },
+    {
+      reacts : [
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="blue">R-COOH</span>+
+          <span class="blue">R'-COOH</span>
+          <div class="equal">
+              <span>-H<sub>2</sub>O</span>
+              <span class="equal__arrow"></span>
+          </div>
+          <span class="s-reaction red">
+              <div>R</div>
+              <div class="divider-1"></div>
+              <div>C</div>
+              <div class="divider-2"></div>
+              <div>O</div>
+          </span>
+          <span class="red">-O-</span>
+          <span class="s-reaction red">
+              <div>R'</div>
+              <div class="divider-1"></div>
+              <div>C</div>
+              <div class="divider-2"></div>
+              <div>O</div>
+          </span>
+        </div>`),
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+            <span class="blue">CH<sub>3</sub>-COOH</span>+
+            <span class="blue">CH<sub>3</sub>-COOH</span>
+            <div class="equal">
+                <span>-H<sub>2</sub>O</span>
+                <span class="equal__arrow"></span>
+            </div>
+            <span class="s-reaction red">
+                <div>CH<sub>3</sub></div>
+                <div class="divider-1"></div>
+                <div>C</div>
+                <div class="divider-2"></div>
+                <div>O</div>
+            </span>
+            <span class="red">-O-</span>
+            <span class="s-reaction red">
+                <div>CH<sub>3</sub></div>
+                <div class="divider-1"></div>
+                <div>C</div>
+                <div class="divider-2"></div>
+                <div>O</div>
+            </span>
+        </div>
+        <p class="text red anhidra">Anhidră Acetică</p>`)
+      ],
+      ex : 0
+    },
+  ]
 
   ngOnInit(): void {
     this.titleService.setTitle("Proprietăți Chimice");
--- src/app/methods-of-obtaining/methods-of-obtaining.component.ts
+++ src/app/methods-of-obtaining/methods-of-obtaining.component.ts
@@ -1,16 +1,215 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-methods-of-obtaining',
   templateUrl: './methods-of-obtaining.component.html',
-  styleUrls: ['./methods-of-obtaining.component.scss']
+  styleUrls: ['./methods-of-obtaining.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class MethodsOfObtainingComponent implements OnInit {
 
-  constructor(private titleService: Title) { }
+  constructor(private titleService: Title, public sanitizer: DomSanitizer) { }
 
   titles : any = []
+
+  R: any = [
+    {
+      reacts : [
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="green">R-CH<sub>3</sub>+</span>
+          <div class="fraction">
+              <span>3</span>
+              <span class="fraction__divider"></span>
+              <span>2</span>
+          </div>
+          <span class="red">O</span><sub>2</sub>→
+          <span class="blue">R-COOH</span>
+          +H<sub>2</sub>O
+        </div>`),
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="green">CH<sub>3</sub>-CH<sub>3</sub>+</span>
+          <div class="fraction">
+              <span>3</span>
+              <span class="fraction__divider"></span>
+              <span>2</span>
+          </div>
+          <span class="red">O</span><sub>2</sub>→
+          <span class="blue">CH<sub>3</sub>-COOH</span>
+          +H<sub>2</sub>O
+        </div>
+        <p class="text">
+            <span class="green">Etan</span>
+            <span class="blue acid-1">Acid Etanoic</span>
+        </p>`)
+      ],
+      ex : 0
+    },
+    {
+      reacts : [
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="purple">R-CH=CH<sub>2</sub></span>+5[<span class="red">O</span>]
+          <div class="equal">
+              <span>KMnO<sub>4</sub></span>
+              <span class="equal__arrow"></span>
+              <span>H<sub>2</sub>SO<sub>4</sub></span>
+          </div>
+          <span class="blue">R-COOH</span>+<span class="purple">CO<sub>2</sub></span>+H<sub>2</sub>O
+        </div>`),
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="purple">CH<sub>3</sub>-CH=CH<sub>2</sub></span>+5[<span class="red">O</span>]
+          <div class="equal">
+              <span>KMnO<sub>4</sub></span>
+              <span class="equal__arrow"></span>
+              <span>H<sub>2</sub>SO<sub>4</sub></span>
+          </div>
+          <span class="blue">CH<sub>3</sub>-COOH</span>+<span class="purple">CO<sub>2</sub></span>+H<sub>2</sub>O
+        </div>
+        <p class="text">
+            <span class="purple">Propenă</span>
+            <span class="blue acid-2">Acid acetic</span>
+        </p>`)
+      ],
+      ex : 0
+    },
+    {
+      reacts : [
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula space-top">
+          <span class="purple">R-CH=</span><span class="blue">CH-R'</span>+4[<span class="red">O</span>]
+          <div class="equal">
+              <span>KMnO<sub>4</sub></span>
+              <span class="equal__arrow"></span>
+              <span>H<sub>2</sub>SO<sub>4</sub></span>
+          </div>
+          <span class="purple">R-COOH</span>+<span class="blue">R'-COOH</span>
+        </div>`),
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+            <span class="purple">CH<sub>3</sub>-CH=</span><span class="blue">CH-CH<sub>2</sub>-CH<sub>3</sub></span>+4[<span class="red">O</span>]
+            <div class="equal">
+                <span>KMnO<sub>4</sub></span>
+                <span class="equal__arrow"></span>
+                <span>H<sub>2</sub>SO<sub>4</sub></span>
+            </div>
+            <span class="purple">CH<sub>3</sub>-COOH</span>+<span class="blue">CH<sub>2</sub>-CH<sub>3</sub>-COOH</span>
+        </div>
+        <p class="text">
+            <span class="purple acid-5">2-pentenă</span>
+            <span class="purple acid-3">Acid Etanoic</span>
+            <span class="blue acid-4">Acid Propanoic</span>
+        </p>`)
+      ],
+      ex : 0
+    },
+    {
+      reacts : [
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="green">R-CH<sub>2</sub>-OH</span>+2[<span class="red">O</span>]
+          <div class="equal">
+              <span>KMnO<sub>4</sub></span>
+              <span class="equal__arrow"></span>
+              <span>H<sub>2</sub>SO<sub>4</sub></span>
+          </div>
+          <span class="green">R-COOH</span>+H<sub>2</sub>O
+        </div>`),
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="green">CH<sub>3</sub>-OH</span>+2[<span class="red">O</span>]
+          <div class="equal">
+              <span>KMnO<sub>4</sub></span>
+              <span class="equal__arrow"></span>
+              <span>H<sub>2</sub>SO<sub>4</sub></span>
+          </div>
+          <span class="green">H-COOH</span>+H<sub>2</sub>O
+        </div>
+        <p class="text green">
+            <span>Metanol</span>
+            <span class="acid-6">Acid Metanoic</span>
+        </p>
+        <div class="formula">
+            <span class="green">CH<sub>3</sub>-CH<sub>2</sub>-OH</span>+2[<span class="red">O</span>]
+            <div class="equal">
+                <span>KMnO<sub>4</sub></span>
+                <span class="equal__arrow"></span>
+                <span>H<sub>2</sub>SO<sub>4</sub></span>
+            </div>
+            <span class="green">CH<sub>3</sub>-COOH</span>+H<sub>2</sub>O
+        </div>
+        <p class="text green">
+            <span>Etanol</span>
+            <span class="acid-7">Acid Etanoic</span>
+        </p>`)
+      ],
+      ex : 0
+    },
+    {
+      reacts : [
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula space-top">
+          <span class="green">R-</span><span class="green tree-3" el="R'">CH</span><span class="green">-OH</span>
+          +4[<span class="red">O</span>]
+          <div class="equal">
+              <span>KMnO<sub>4</sub></span>
+              <span class="equal__arrow"></span>
+              <span>H<sub>2</sub>SO<sub>4</sub></span>
+          </div>
+          <span class="green">R-COOH</span>+
+          <span class="green">R'-COOH</span>+H<sub>2</sub>O
+        </div>
+        <div style="margin-top: 4rem;"></div>`),
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="green">CH<sub>3</sub>-CH<sub>2</sub>-</span><span class="green tree-3" el="OH">CH</span><span class="green">-CH<sub>3</sub></span>
+          +4[<span class="red">O</span>]
+          <div class="equal">
+              <span>KMnO<sub>4</sub></span>
+              <span class="equal__arrow"></span>
+              <span>H<sub>2</sub>SO<sub>4</sub></span>
+          </div>
+          <span class="green">CH<sub>3</sub>-CH<sub>2</sub>-COOH</span>+
+          <span class="green">H-COOH</span>+H<sub>2</sub>O
+        </div>
+        <p class="text green">
+            <span class="butanol">2-Butanol</span>
+            <span class="acid-8">Acid Propanoic</span>
+            <span class="acid-9">Acid Metanoic</span>
+        </p>`)
+      ],
+      ex : 0
+    },
+    {
+      reacts : [
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="blue">R</span>-C<span class="teal">X<sub>3</sub></span>+2
+          <span class="red">H-OH</span>→<span class="blue">R-COOH</span>+3H<span class="teal">X</span>
+        </div>`),
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="blue">CH<sub>3</sub></span>-C<span class="teal">Cl<sub>3</sub></span>+2
+          <span class="red">H-OH</span>→<span class="blue">CH<sub>3</sub>-COOH</span>+3H<span class="teal">Cl</span>
+        </div>
+        <p class="text blue">
+            <span>1,1,1 tricloro-etan</span>
+            <span class="acid-10">Acid Etanoic</span>
+        </p>`)
+      ],
+      ex : 0
+    },
+    {
+      reacts : [
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="lime">R-COO</span><span class="blue">R'</span>+<span class="red">H<sub>2</sub>O</span>
+          ⇄<span class="lime">R-COOH</span>+<span class="blue">R'-OH</span>
+        </div>`),
+        this.sanitizer.bypassSecurityTrustHtml(`<div class="formula">
+          <span class="lime">CH<sub>3</sub>-COO</span><span class="blue">CH<sub>3</sub></span>+<span class="red">H<sub>2</sub>O</span>
+          ⇄<span class="lime">CH<sub>3</sub>-COOH</span>+<span class="blue">CH<sub>3</sub>-OH</span>
+        </div>
+        <p class="text">
+            <span class="lime">Acetat de Metil</span>
+            <span class="lime acid-11">Acid Metanoic</span>
+            <span class="blue metanol">Metanol</span>
+        </p>`)
+      ],
+      ex : 0
+    }
+  ]
 
   ngOnInit(): void {
     this.titleService.setTitle("Metode de obținere");
